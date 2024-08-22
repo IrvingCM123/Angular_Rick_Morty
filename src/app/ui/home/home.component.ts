932311a4b5be7b7166ee8fdb258903f02a62319c
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-08-22 00:25:23 -0600

    paginar personajes

--- src/app/ui/home/home.component.ts
+++ src/app/ui/home/home.component.ts
@@ -7,68 +7,111 @@ import { firstValueFrom } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   constructor(
     private personajesUsecase: PersonajesUsecase,
-    private cacheService: Cache_Service,
-  ) { }
+    private cacheService: Cache_Service
+  ) {}
 
   // Arreglo de personajes
   personajes: personaje[] = [];
 
   // Variables para el paginador
+  personajesPagina: personaje[] = [];
   paginaActual: number = 1;
   personajesPorPagina: number = 10;
   totalPersonajes: number = 0;
+  totalPaginas: number = 0;
 
-  ngOnInit(): void {
-  }
+  cargando: boolean = false;
+
+  ngOnInit(): void {}
 
   // Método que obtiene los personajes de la API a través del puerto y los guarda en el cache
   async obtenerPersonajes(): Promise<personaje[]> {
-      // Si no están en el cache, los obtiene de la API y los guarda en el cache
-      const datos: any = await firstValueFrom(this.personajesUsecase.obtenerPersonajes());
-      this.personajes = datos.body;
-      this.totalPersonajes = this.personajes.length;
-      return this.personajes;
+    const datos: any = await firstValueFrom(
+      this.personajesUsecase.obtenerPersonajes()
+    );
+    this.personajes = datos.body;
+    this.totalPersonajes = this.personajes.length;
+    this.totalPaginas = Math.ceil(
+      this.totalPersonajes / this.personajesPorPagina
+    ); // Calcular el total de páginas
+    return this.personajes;
   }
 
   // Método para obtener un personaje específico por su nombre
   async obtenerPersonajePorNombre(nombre: string): Promise<personaje[]> {
+    this.cargando = true;
+    try {
       // Si no está en el cache, lo obtiene de la API y lo guarda en el cache
-      const datos: any = await firstValueFrom(this.personajesUsecase.obtenerPersonajePorNombre(nombre));
+      const datos: any = await firstValueFrom(
+        this.personajesUsecase.obtenerPersonajePorNombre(nombre)
+      );
       this.personajes = datos.body;
-      this.totalPersonajes = this.personajes.length;
-      return this.personajes
+
+      // Calcular el índice de inicio y fin para la paginación
+      const indiceInicio = (this.paginaActual - 1) * this.personajesPorPagina;
+      const indiceFin = indiceInicio + this.personajesPorPagina;
+
+      // Actualizar los personajes de la página actual
+      this.personajesPagina = this.personajes.slice(indiceInicio, indiceFin);
+    } catch (error) {
+      console.error('Error al obtener el personaje', error);
+    } finally {
+      this.cargando = false;
+      return this.personajes;
+    }
   }
 
   // Método para obtener un personaje específico por su id
   async obtenerPersonajePorId(id: string): Promise<personaje[]> {
-      const datos: any = await firstValueFrom(this.personajesUsecase.obtenerPersonajePorId(id));
+    this.cargando = true;
+
+    try {
+      const datos: any = await firstValueFrom(
+        this.personajesUsecase.obtenerPersonajePorId(id)
+      );
       this.personajes = datos.body;
-      return this.personajes
+
+      // Calcular el índice de inicio y fin para la paginación
+      const indiceInicio = (this.paginaActual - 1) * this.personajesPorPagina;
+      const indiceFin = indiceInicio + this.personajesPorPagina;
+
+      // Actualizar los personajes de la página actual
+      this.personajesPagina = this.personajes.slice(indiceInicio, indiceFin);
+    } catch (error) {
+      console.error('Error al obtener el personaje', error);
+    } finally {
+      this.cargando = false;
+      return this.personajes;
+    }
   }
 
-  // Método para obtener los personajes de la página actual
-  async obtenerPersonajesPagina(): Promise<personaje[]> {
-    // Obtiene los personajes de la API
-    this.personajes = await this.obtenerPersonajes();
-    console.log(this.personajes, "2");
-    // Calcula el índice de inicio y fin de los personajes de la página actual
-    const indiceInicio = (this.paginaActual - 1) * this.personajesPorPagina;
-    const indiceFin = indiceInicio + this.personajesPorPagina;
-    // Devuelve los personajes de la página actual
-    return this.personajes.slice(indiceInicio, indiceFin);
+  async obtenerPersonajesPagina(): Promise<void> {
+    this.cargando = true;
+    try {
+      await this.obtenerPersonajes();
+
+      // Calcular el índice de inicio y fin para la paginación
+      const indiceInicio = (this.paginaActual - 1) * this.personajesPorPagina;
+      const indiceFin = indiceInicio + this.personajesPorPagina;
+
+      // Actualizar los personajes de la página actual
+      this.personajesPagina = this.personajes.slice(indiceInicio, indiceFin);
+    } catch (error) {
+      console.error('Error al obtener los personajes', error);
+    } finally {
+      this.cargando = false;
+    }
   }
 
-  // Método para cambiar de página
   cambiarPagina(pagina: number): void {
-    this.paginaActual = pagina;
-    this.obtenerPersonajesPagina();
+    if (pagina > 0 && pagina <= this.totalPaginas) {
+      this.paginaActual = pagina;
+      this.obtenerPersonajesPagina(); // Actualizar los personajes de la página actual
+    }
   }
-
-
 }
